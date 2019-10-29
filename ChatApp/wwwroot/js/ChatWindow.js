2ac2bf5c004dd1b6a0a5e496a29e873bf8b611c4
Matthew Gardiner <MatthewGardiner1@outlook.com>
2019-10-29 17:31:45 +1000

    Added client side message validation

--- ChatApp/wwwroot/js/ChatWindow.js
+++ ChatApp/wwwroot/js/ChatWindow.js
@@ -17,6 +17,14 @@
 
         $(this.btnSend).click(this.btnSend_Click.bind(this));
         $(this.txtMessageInput).keydown(this.txtMessageInput_Keydown.bind(this));
+
+        //var parent = this;
+        setInterval(function (parent) {
+            if (Boolean($(parent.txtMessageInput).val()) === false)
+                $(parent.btnSend).removeClass("btn-primary").addClass("btn-disabled");
+            else
+                $(parent.btnSend).removeClass("btn-disabled").addClass("btn-primary");
+        }, 125,this);
     }
 
     //Properties
@@ -31,9 +39,10 @@
 
     btnSend_Click() {
         var contents = $(this.txtMessageInput).val();
+        if (Boolean(contents) === false)
+            return;
 
         $(this.txtMessageInput).val("").focus();
-
 
         //Create a temporary message row so there is no delay in displaying a user message while waiting for the server to return an ID
         this.CreateTempMessageRow(contents);
